4f2650821772e4c18141c24a5f4fc292a4028842
M-44ks <1maksymadamiuk1@gmail.com>
2024-12-22 18:52:51 +0100

    Add profile screen and create habit screen with placeholder content

---
+++ lib/screens/create_habit_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateHabitScreen extends StatelessWidget {
+  const CreateHabitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
