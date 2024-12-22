eeb0583322ce3d45156b59a78ccaa81f8147eba9
M-44ks <1maksymadamiuk1@gmail.com>
2024-12-22 20:18:40 +0100

    Add screens package

--- lib/auth/screens/login_screen.dart
+++ lib/auth/screens/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_habit_tracker/home_screen.dart';
+import 'package:smart_habit_tracker/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
