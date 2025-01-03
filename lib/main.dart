54394799ba5d2f912fd1a447c3c0ec62b2275bf4
M-44ks <1maksymadamiuk1@gmail.com>
2025-01-03 23:00:23 +0100

    Add AuthenticationRouter to check user login status

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -25,7 +26,29 @@ class MyApp extends StatelessWidget {
           backgroundColor: T.white_1,
         ),
       ),
-      home: const MainNavigation(),
+      home: const AuthenticationRouter(),
+    );
+  }
+}
+
+class AuthenticationRouter extends StatelessWidget {
+  const AuthenticationRouter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasData) {
+          return const MainNavigation();
+        } else {
+          return const LoginRegisterScreen();
+        }
+      },
     );
   }
 }
